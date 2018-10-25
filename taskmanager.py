c678913777fba2fb5ace9666880b8414e62d0375
Richard Dalton <richard@codeinstitute.net>
2018-10-25 14:04:00 +0000

    Connected to Mongo

--- taskmanager.py
+++ taskmanager.py
@@ -1,11 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
 import os
+from flask_pymongo import PyMongo
+
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+
+mongo = PyMongo(app)
+
 @app.route("/")
 def show_hi():
-    return "Hi"
+    tasks = mongo.db.tasks.find()
+    return render_template("tasks.html", tasks=tasks)
 
 if __name__ == "__main__":
         app.run(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 8080)), debug=True)
