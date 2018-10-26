19fa751057b39d18f8eb840a5fa3f1a7eba49ba1
Richard Dalton <richard@codeinstitute.net>
2018-10-26 09:31:13 +0000

    Did Mongo Right

--- taskmanager.py
+++ taskmanager.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 import os
 from flask_pymongo import PyMongo
 
@@ -11,9 +11,33 @@ app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 mongo = PyMongo(app)
 
 @app.route("/")
-def show_hi():
+def get_tasks():
     tasks = mongo.db.tasks.find()
     return render_template("tasks.html", tasks=tasks)
+
+
+@app.route("/tasks/<collection_name>")
+def get_tasks_by_collection(collection_name):
+    tasks = mongo.db[collection_name].find()
+    return render_template("tasks.html", tasks=tasks)
+
+
+
+@app.route("/add_task", methods=["GET", "POST"])
+def add_task():
+    if request.method=="POST":
+        form_values = request.form.to_dict()
+        form_values["is_urgent"] = "is_urgent" in form_values
+        category = form_values["category_name"]
+        mongo.db[category].insert_one(form_values)
+        return redirect("/")
+    else:
+        categories = []
+        for category in mongo.db.collection_names():
+            if not category.startswith("system."):
+                categories.append(category)
+        
+        return render_template("addtask.html", categories=categories)
 
 if __name__ == "__main__":
         app.run(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 8080)), debug=True)
